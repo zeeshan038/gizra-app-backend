e77f8fab091a72a740d6cc8718ea6ebae6f6caf8
Zeeshan <hankhan123623@gmail.com>
2026-09-12 16:26:01 +0500

    fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+//Test Connection
 export const testConnection = async () => {
   try {
     await prisma.$connect();
